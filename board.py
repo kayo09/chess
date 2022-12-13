2acccfc4ac3a6849a2a2d9b60c55b7dce3cb1fb8
kayo09 <68217041+kayo09@users.noreply.github.com>
2022-12-13 17:54:30 -0500

    added board

--- board.py
+++ board.py
@@ -1,2 +1,15 @@
 class board:
-    board_mat = [][]
+    board_mat = [8][8]
+
+
+# checks for peice potential moves compatibility
+def potential_moves_for_peices(obj):
+    where_peice_is(obj)  # returns position
+
+
+def where_peice_is(obj):
+    obj.location
+
+
+def valid_moves():  # just for board compatibility
+    pass  # move has to be within range of the board
