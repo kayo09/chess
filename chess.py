984418f17b589473e21e7838140fd8772aadb1d9
kayo09 <68217041+kayo09@users.noreply.github.com>
2022-12-13 17:54:55 -0500

    Update chess.py

---
+++ chess.py
@@ -0,0 +1,14 @@
+class king:
+    location
+    max_move_length
+    team
+    is_alive
+
+    def possible_moves():
+        board_obj
+
+    def travel():
+        pass
+
+    def where_on_board():
+        prints("k" at location)
